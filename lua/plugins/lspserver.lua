0eb4ba5d73096a186f851f6586a9298e703ee465
Vegar Karlsen <vegar@live.no>
2023-09-20 13:55:00 +0200

    template for disable and lpsserver, ignore themes and package*.json files

---
+++ lua/plugins/lspserver.lua
@@ -0,0 +1,19 @@
+if true then
+    return {}
+end
+
+return {
+    -- add pyright to lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        ---@class PluginLspOpts
+        opts = {
+            ---@type lspconfig.options
+            servers = {
+                -- pyright will be automatically installed with mason and loaded with lspconfig
+                pyright = {},
+                clangd = {},
+            },
+        },
+    },
+}
