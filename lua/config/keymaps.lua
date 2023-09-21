6d04f1ba41c8e7216a7d3549f6eb6cb1f6938fdd
Vegar Karlsen <vegar@live.no>
2023-09-21 23:58:12 +0200

    merged gitignore

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
@@ -12,3 +13,5 @@ vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous
 
 -- map H ^
 -- map L $
+=======
+>>>>>>> Stashed changes
