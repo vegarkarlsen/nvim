5358246c05acca1b46bcbbbaa40fadc57660d683
Vegar Karlsen <vegar@live.no>
2023-09-21 23:55:13 +0200

    new keybinding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- change keybinding for moving between windows
 vim.keymap.del("n", "<leader><tab>[")
 vim.keymap.del("n", "<leader><tab>]")
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+
+-- move line start and line ends
+
+-- map H ^
+-- map L $
