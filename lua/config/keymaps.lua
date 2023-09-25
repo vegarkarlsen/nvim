7a1971c02633a6cdee2a9d11980f68dbfa109158
Vegar Karlsen <vegar@live.no>
2023-09-25 19:18:33 +0200

    disable :q

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,6 +8,9 @@ vim.keymap.del("n", "<leader><tab>]")
 vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnext<cr>", { desc = "Next Tab" })
 vim.keymap.set("n", "<leader><tab>p", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
 
+-- Disable q: for opening cmd-history
+vim.keymap.set("n", "q:", "", { desc = "Hack for disabeling cmd-history on this binding" })
+
 -- move line start and line ends
 
 -- map H ^
