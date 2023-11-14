70fe4483cd966f08cd4b3f5f0046aabdc2e5dbf9
Vegar Karlsen <vegar@live.no>
2023-11-14 01:17:27 +0100

    Add tresitter config.

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,32 @@
+return {
+
+    {
+        "nvim-treesitter/nvim-treesitter",
+        opts = {
+            ensure_installed = {
+                "bash",
+                "html",
+                "css",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "tsx",
+                "typescript",
+                "vim",
+                "yaml",
+                "c",
+                "cpp",
+                "gitignore",
+                "htmldjango",
+                "make",
+                "ssh_config"
+            },
+        },
+    },
+
+}
