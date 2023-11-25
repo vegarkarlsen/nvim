d96c22fa544b8437976a67c52c9fb40ae90a7815
Vegar Karlsen <vegar@live.no>
2023-11-25 01:27:06 +0100

    Add some more lanugages to parase in tressitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,19 @@
+-- Tresitter is for parsing colors?
+
 return {
 
     {
         "nvim-treesitter/nvim-treesitter",
         opts = {
             ensure_installed = {
+                "perl",
+                "matlab",
                 "bash",
                 "html",
                 "css",
                 "javascript",
                 "json",
+                "json5",
                 "lua",
                 "markdown",
                 "markdown_inline",
@@ -24,9 +29,15 @@ return {
                 "gitignore",
                 "htmldjango",
                 "make",
-                "ssh_config"
+                "ssh_config",
+                "cmake",
+                "latex",
+                "puppet",
+                "regex",
+                "rust",
+                "sql",
+                "xml",
             },
         },
     },
-
 }
