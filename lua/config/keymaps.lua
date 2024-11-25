bb4a71c00a12052fe3430ed35eab9f07fe1195df
Vegar <Vegar1201@live.no>
2024-11-25 13:08:20 +0100

    Fix q* disable and add delete marker keybinding.
    We now mark with mm, and delete mark with md.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,10 +7,10 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Disable native cmd-history keybindings, <space>: is better
-vim.keymap.set("n", "q:", "nop")
-vim.keymap.set("n", "q?", "Nop")
-vim.keymap.set("n", "q/", "Nop")
-vim.keymap.set("n","C-f", "Nop")
+vim.keymap.set("n", "q:", "<Nop>")
+vim.keymap.set("n", "q?", "<Nop>")
+vim.keymap.set("n", "q/", "<Nop>")
+vim.keymap.set("n","C-f", "<Nop>")
 
 -- Dissable the bad suggestions not that popes up
 vim.keymap.set("i", "<C-n>", "<Nop>")
@@ -19,3 +19,10 @@ vim.keymap.set("i", "<C-p>", "<Nop>")
 -- Create new buffer
 vim.keymap.set("n", "<leader>bn", "<cmd>ene<cr>", { desc = "New buffer" })
 
+-- marks
+vim.keymap.set("n", "mm", "m", { desc = "Mark" })
+vim.keymap.set("n", "md", ":delmark ", { desc = "Delete mark" })
+
+-- Motion
+-- vim.keymap.set({ "n", "v" }, "gs", "g^", { desc = "Go to start of line " })
+-- vim.keymap.set({ "n", "v" }, "gl", "g$", { desc = "Go to end of line " })
